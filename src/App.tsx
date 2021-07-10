ce1e9b7be2c701b8124adbaee30025a3bd6be059
Hchaichi94 <hchaichi-akrem@outlook.fr>
2021-07-10 15:32:36 +0100

    add api file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Difficulty, fecthQuizQuestions } from './API';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Total_Questions = 15
+const App = () => {
+
+  const [Loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState(([]))
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  console.log(fecthQuizQuestions(Total_Questions, Difficulty.EASY))
+  const startTrivia = async () => { }
+
+
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => { }
+
+  const nextQuestion = async () => { }
+
+  return <div className="App">
+
+    <h1>React Quiz</h1>
+    <button className="start" onClick={startTrivia}>start</button>
+    <p>Loading questions ...</p>
+    {/* <QuestionCard
+      questionNr={number + 1}
+      totalQuestions={Total_Questions}
+      answers={questions[number].answers}
+      question={questions[number].question}
+      userAnswer={userAnswers ? userAnswers[number] : undefined}
+      callback={checkAnswer}
+    /> */}
+    <button className="next" onClick={nextQuestion}>next</button>
+  </div>
+
 }
 
 export default App;
